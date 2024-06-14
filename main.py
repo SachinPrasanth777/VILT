9c0902c09a837a498e1c7018680746864a9ba2d9
Sachin Prasanth <142246653+SachinPrasanth777@users.noreply.github.com>
2024-06-14 05:30:51 +0000

    fix: add middleware

--- main.py
+++ main.py
@@ -3,8 +3,17 @@ from fastapi.responses import JSONResponse
 from PIL import Image, UnidentifiedImageError
 import io
 from model import model_pipeline
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
 
 @app.get('/')
 async def page():
